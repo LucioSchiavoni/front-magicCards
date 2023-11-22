40a1839f161e543ea0c262e223b05b50a8c8387a
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-11-22 18:30:07 -0300

    Feat: se hace el buscador y el create desde el cliente

--- src/CreateCard.jsx
+++ src/CreateCard.jsx
@@ -1,12 +1,68 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { gql, useMutation } from '@apollo/client'
+import { useFormik } from 'formik';
+
+const CREATE_CARD_MUTATION = gql`
+mutation createCard($name: String!, $description: String!, $points: String!){
+  createCard(create: {
+    name: $name
+    description: $description
+    points: $points
+  }) {
+    name 
+    description
+    points
+  }
+}
+`;
 
 const CreateCard = () => {
+
+const [createCardMutation] = useMutation(CREATE_CARD_MUTATION)
+
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      description: '',
+      points: '',
+    },
+
+    onSubmit: async (values) => {
+        try {
+          console.log(values)
+          const {data} = createCardMutation({
+            variables: values
+          })
+          
+
+        } catch (error) {
+          console.log(error)
+        }
+    }
+  })
+
+
   return (
 
     <div>
         <Link to='/' className="absolute left-5 top-5 bg-red-800 px-4 py-1 rounded-lg text-white ">Inicio</Link>
-        <p>Crear</p>
+        <form onSubmit={formik.handleSubmit} method='POST' className='mt-24 p-6 bg-white rounded-md space-y-10'>
+          <div className='flex flex-col'>
+            <label htmlFor="" className='text-black'>Nombre de la carta</label>
+            <input type="text" id='name' className='px-3 py-1 text-black placeholder:italic rounded-md bg-gray-100 ring-2 ring-red-800 focus:outline-none' value={formik.values.name} onChange={formik.handleChange} placeholder='Nombre' required/>
+          </div>
+          <div className='flex flex-col'>
+             <label htmlFor="" className='text-black'>Descripcion</label>
+            <textarea type="text" id='description' className='px-3 py-1 text-black placeholder:italic rounded-md bg-gray-100 ring-2 ring-red-800 focus:outline-none' placeholder='Descripcion de la carta' value={formik.values.description} onChange={formik.handleChange} required></textarea>
+          </div>
+          <div className='flex flex-col'>
+             <label htmlFor="" className='text-black'>Puntos</label>
+            <input type="text" id='points' className='px-3 py-1 text-black placeholder:italic rounded-md bg-gray-100 ring-2 ring-red-800 focus:outline-none' value={formik.values.points} placeholder='Puntos' onChange={formik.handleChange} required/>
+          </div>
+          <button className='px-6 py-1 rounded-md bg-red-800 text-white text-xl' type='submit'>Guardar</button>
+        </form>
     </div>
   )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import { useQuery, gql } from "@apollo/client"
 import { GET_CARDS } from "./graphql/graphql"
 import { Link } from "react-router-dom";
+import { useState } from "react";
 const Home = () => {
 
 
@@ -9,20 +10,29 @@ const Home = () => {
 
     const cards = data?.cards ;
 
+    const [search, setSearch] = useState('')
+
 
     if(loading) return <div><p className="text-white 2xl text-center">Cargando...</p></div>
     if(error) return <div><p className="text-white 2xl text-center">{error.message}</p></div>
 
+    const filterCards = cards.filter(card => 
+        card.name.toLowerCase().includes(search.toLowerCase()))
 
 
     return (
-    <div className="min-h-screen  flex justify-center items-center flex-col">
-        <Link to='/create' className="text-white bg-red-800 px-4  py-1 rounded-md absolute right-8 top-10">Crear</Link>
-        <h1 className="mb-2 font-semibold text-3xl">Buscar carta</h1>
-        <input type="text" className="bg-white rounded-md px-6 py-1 mb-8 text-black focus:ring-2 focus:ring-red-800 focus:outline-none" placeholder="Nombre de la carta.." />
-            
+    <div className="min-h-screen flex justify-center items-center flex-col">
+        <Link to='/create' className="text-white bg-red-800 px-4  py-1 rounded-md absolute right-3 top-3">Crear</Link>
+        <h1 className="mb-2 font-semibold text-3xl text-white">Buscar carta</h1>
+        <input type="text" className="bg-white rounded-md px-6 py-1 mb-8 text-black focus:ring-2 focus:ring-red-800 focus:outline-none" placeholder="Nombre de la carta.." 
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        />
+            <div className="flex flex-col gap-10">
+
+           
             {
-                cards.map((item, index) => (
+                filterCards.map((item, index) => (
                     <div key={index} className=" p-8 font-semibold bg-white text-black rounded-lg ">
                         <h2 className="text-2xl mb-5">{item.name}</h2>
                         <p className="">{item.description}</p>
@@ -31,7 +41,7 @@ const Home = () => {
 
                 ))
             }
-    </div>
+    </div> </div>
     )
 }
 
