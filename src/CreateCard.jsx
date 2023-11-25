06b264aa2ef0fda6215d59cbea3b5728ce71205f
Lucio Schiavoni <104010031+LucioSchiavoni@users.noreply.github.com>
2023-11-25 11:38:08 -0300

    input de puntos no es requerido

--- src/CreateCard.jsx
+++ src/CreateCard.jsx
@@ -59,7 +59,7 @@ const [createCardMutation] = useMutation(CREATE_CARD_MUTATION)
           </div>
           <div className='flex flex-col'>
              <label htmlFor="" className='text-black'>Puntos</label>
-            <input type="text" id='points' className='px-3 py-1 text-black placeholder:italic rounded-md bg-gray-100 ring-2 ring-red-800 focus:outline-none' value={formik.values.points} placeholder='Puntos' onChange={formik.handleChange} required/>
+            <input type="text" id='points' className='px-3 py-1 text-black placeholder:italic rounded-md bg-gray-100 ring-2 ring-red-800 focus:outline-none' value={formik.values.points} placeholder='Puntos' onChange={formik.handleChange} />
           </div>
           <button className='px-6 py-1 rounded-lg bg-red-800 text-white text-xl' type='submit'>Guardar</button>
         </form>
